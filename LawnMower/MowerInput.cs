f597d0a4b935864b9bd36b868d639340d5399d48
Chandrasekhar.Tammi <Chandrasekhar.Tammi@TS-CHANDRASEKHA>
2017-10-19 14:11:56 +0100

    Add project files.

---
+++ LawnMower/MowerInput.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnMower
+{
+
+    public enum MowerCommand { Move, Left, Right };
+    public enum CompassDirection { North, South, East, West };
+
+    public class MowerInput
+    {
+        public int GridX { get; set; }
+        public int GridY { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public CompassDirection Direction { get; set; }
+        public List<MowerCommand> Commands { get; set; }
+
+        public MowerInput ParseMowerInput(string gridDimensionsAndStartingDirection, string commands, string gridSize)
+        {
+            MowerInput moverInput = new MowerInput();
+            int temp;
+            string[] gridcoordinates = gridSize.Split(' ');
+          
+            if (int.TryParse(gridcoordinates[0], out temp))
+            {
+                moverInput.GridX = temp;
+            }
+
+            if (int.TryParse(gridcoordinates[1], out temp))
+            {
+                moverInput.GridY = temp;
+            }
+          
+            string[] data = gridDimensionsAndStartingDirection.Split(' ');
+            if (int.TryParse(data[0], out temp))
+            {
+                moverInput.X = temp;
+            }
+
+            if (int.TryParse(data[1], out temp))
+            {
+                moverInput.Y = temp;
+            }
+
+            string cdirection = data[2];
+            char cc;
+            cc = cdirection.ToCharArray()[0];
+            moverInput.Direction = ParseCompassDirection(cc);
+
+            List<MowerCommand> mowerCommands = new List<MowerCommand>();
+
+            var chars = commands.ToCharArray();
+            foreach (char c in chars)
+            {
+                MowerCommand Commands = ParseMowerCommand(c);
+                mowerCommands.Add(Commands);
+            }
+            moverInput.Commands = mowerCommands;
+
+            return moverInput;
+        }
+
+        private MowerCommand ParseMowerCommand(char command)
+        {
+            switch (command)
+            {
+                case 'M':
+                    return MowerCommand.Move;
+                case 'L':
+                    return MowerCommand.Left;
+                case 'R':
+                    return MowerCommand.Right;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private CompassDirection ParseCompassDirection(char directionAbbreviation)
+        {
+            switch (directionAbbreviation)
+            {
+                case 'N':
+                    return CompassDirection.North;
+                case 'S':
+                    return CompassDirection.South;
+                case 'E':
+                    return CompassDirection.East;
+                case 'W':
+                    return CompassDirection.West;
+                default:
+                    throw new ArgumentException("Not a valid direction");
+            }
+        }
+    }
+       
+}
---
+++ LawnMower/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnMower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MowerInput mi = new MowerInput();
+            //string[] coordinates1;
+            string gridSize = Console.ReadLine();
+            //Console.WriteLine("Grid Size " + gridSize);
+            //string[] coordinates = gridSize.Split(' ');
+            //int x, y;
+            //Int32.TryParse(coordinates[0], out x);
+            //Int32.TryParse(coordinates[1], out y);
+            //Console.WriteLine("x-axis " + x);
+            //Console.WriteLine("y-axis " + y);
+
+            List<string> moverinput = new List<string>();
+
+            string input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                moverinput.Add(input);
+                input = Console.ReadLine();
+            }
+           
+
+            if (moverinput.Count > 0)
+            {
+                List<MowerInput> allParsedInputs = new List<MowerInput>();
+                for (int i = 0; i < moverinput.Count; i++)
+                {
+                    var test = mi.ParseMowerInput(moverinput[i], moverinput[i + 1], gridSize);
+                    allParsedInputs.Add(test);
+                    i += 1;
+                }
+                //allParsedInputs
+
+                foreach (var parsedInput in allParsedInputs)
+                {
+                    Console.WriteLine($"GridCoordinates:({parsedInput.GridX}, {parsedInput.GridY}) Coordinates:({parsedInput.X}, {parsedInput.Y}) Direction: {parsedInput.Direction} Commands: {parsedInput.Commands[0]} {parsedInput.Commands[1]} {parsedInput.Commands[2]} {parsedInput.Commands[3]}");
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
